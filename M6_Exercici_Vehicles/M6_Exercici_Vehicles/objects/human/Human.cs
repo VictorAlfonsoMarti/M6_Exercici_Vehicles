20c76df71c83e83ee289696892097b31cd57df0d
JoanJuan10 <iijoanjuan10@gmail.com>
2021-01-11 13:17:04 +0100

    Fase 2

---
+++ M6_Exercici_Vehicles/M6_Exercici_Vehicles/objects/human/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M6_Exercici_Vehicles
+{
+    public class Human
+    {
+        public Human ()
+        {
+
+        }
+    }
+}
---
+++ M6_Exercici_Vehicles/M6_Exercici_Vehicles/objects/human/License.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M6_Exercici_Vehicles
+{
+    public class License
+    {
+        private string ID;
+        private string tipus;
+        private string nomComplet;
+        private string caducitat;
+
+        public License(string iD, string tipus, string nomComplet, string caducitat)
+        {
+            ID = iD;
+            this.tipus = tipus;
+            this.nomComplet = nomComplet;
+            this.caducitat = caducitat;
+        }
+        public License ()
+        {
+        }
+
+        public string ID1 { get => ID; set => ID = value; }
+        public string Tipus { get => tipus; set => tipus = value; }
+        public string NomComplet { get => nomComplet; set => nomComplet = value; }
+        public string Caducitat { get => caducitat; set => caducitat = value; }
+    
+
+    }
+}
